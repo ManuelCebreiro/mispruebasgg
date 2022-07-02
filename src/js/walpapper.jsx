df93ffd9ec5f08e6195c9311873c83b45a1dbe0a
ManuelCebreiro <cebreirom@gmail.com>
2022-07-02 21:17:08 +0000

    probando 1.0

---
+++ src/js/walpapper.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Wallpapper = (props) => {
+    return (
+    <div>
+            <h1 id="text" className="atari">Bienvenido</h1>
+            <button onClick={this.disable}  id="btn" className="atari">Start</button>
+    </div >
+    )
+}
+
+export default Wallpapper
+
+
